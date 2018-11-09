334762a9f0292f831ea79800b31289083c340630
Connor Ott <connor.ott@colorado.edu>
2018-11-09 10:37:09 -0700

    eat it brendan

--- CR3BP_main.m
+++ CR3BP_main.m
@@ -16,7 +16,9 @@ t_end = 'many'; % [s]
 t_span = [t_0, t_end];
 
 
+'oooooooohhhh changes????'
 
-[t_s, F_s] = ode113(@(t, F)cr3bp_eom(t, F, mu), t_span, );
+% [t_s, F_s] = ode113(@(t, F)cr3bp_eom(t, F, mu), t_span, );
+
 
 
